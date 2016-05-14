85eaef6dff0ff6a80e70a79e22133b004c23ab6a
TenPlus1 <kinsellaja@yahoo.com>
2016-05-14 08:40:11 +0100

    fixed typo, rats can be fed to kittens once more

--- kitten.lua
+++ kitten.lua
@@ -38,7 +38,7 @@ mobs:register_mob("mobs_animal:kitten", {
 		walk_start = 0,
 		walk_end = 96,
 	},
-	follow = {"mobs:rat", "ethereal:fish_raw", "mobs_fish:clownfish", "mobs_fish:tropical"},
+	follow = {"mobs_animal:rat", "ethereal:fish_raw", "mobs_fish:clownfish", "mobs_fish:tropical"},
 	view_range = 8,
 	on_rightclick = function(self, clicker)
 
