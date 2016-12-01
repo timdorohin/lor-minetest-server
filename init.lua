5fbc7dd57d87c1798310b8f2a98fbe3361de7c7e
TenPlus1 <kinsellaja@yahoo.com>
2016-12-01 18:41:27 +0000

    Added 9 lucky blocks

--- init.lua
+++ init.lua
@@ -2,12 +2,15 @@
 local path = minetest.get_modpath("mobs")
 
 -- Mob API
-dofile(path.."/api.lua")
+dofile(path .. "/api.lua")
 
 -- Mob Items
-dofile(path.."/crafts.lua")
+dofile(path .. "/crafts.lua")
 
--- Spawner
-dofile(path.."/spawner.lua")
+-- Mob Spawner
+dofile(path .. "/spawner.lua")
+
+-- Lucky Blocks
+dofile(path .. "/lucky_block.lua")
 
 print ("[MOD] Mobs Redo loaded")
